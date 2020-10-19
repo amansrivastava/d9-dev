10d1af01db77d9b271a477ac80592ec2bd39be91
vagrant <vagrant@local.d9-dev.com>
2020-10-19 20:03:20 +0000

    D9-03: Create custom config form.

---
+++ docroot/modules/custom/mymodule/src/Form/CustomSettingsForm.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\mymodule\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configure mymodule settings for this site.
+ */
+class CustomSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'mymodule_custom_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['mymodule.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('mymodule.settings');
+    $form['custom_name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Custom Name'),
+      '#default_value' => $config->get('custom_name'),
+    ];
+    $form['custom_checkbox'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('This is a checkbox example.'),
+      '#default_value' => $config->get('custom_checkbox'),
+    ];
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save Custom Configs'),
+      '#button_type' => 'primary',
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('mymodule.settings')
+      ->set('custom_name', $form_state->getValue('custom_name'))
+      ->set('custom_checkbox', $form_state->getValue('custom_checkbox'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
