7f162f0f4b30ddf1d7c7f7f7c8adcf996c29a569
Aman Srivastava <aman.srivastava@live.com>
2020-10-21 11:26:26 +0530

    D9-03: fix permissions and add comment.

--- docroot/modules/custom/mymodule/src/Form/CustomSettingsForm.php
+++ docroot/modules/custom/mymodule/src/Form/CustomSettingsForm.php
@@ -52,7 +52,9 @@ class CustomSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-
+    /**
+     * @TODO implement Validation on form values.
+     */
     parent::validateForm($form, $form_state);
   }
 
