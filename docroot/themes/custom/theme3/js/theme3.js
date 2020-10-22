e84c647e444267c259b28c9e560c674791091277
Aman Srivastava <aman.srivastava@acquia.com>
2020-10-22 14:16:18 +0530

    D9-12: Create a theme with 3 columns layout. (#17)
    * D9-12: Create a theme with 3 columns layout.
    
    * D9-12: remove unused files.

---
+++ docroot/themes/custom/theme3/js/theme3.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * theme3 behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.theme3 = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
