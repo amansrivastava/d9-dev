57bd72869d72db9dcbcec84d9287cf0a5d3927a5
Aman Srivastava <aman.srivastava@acquia.com>
2020-10-29 22:07:20 +0530

    #19 - Add given module to codebase. (#36)
    * D9-19: Add given module to code.

---
+++ docroot/modules/custom/day19/src/Plugin/Block/MyBlock.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\day19\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'MyBlock' block.
+ *
+ * @Block(
+ *  id = "my_block",
+ *  admin_label = @Translation("My block"),
+ * )
+ */
+class MyBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $some_array = [
+      1 => ['a' => 'mango', 'b' => 'guava', 'c' => 'jack fruit'],
+      2 => ['d' => 'banana', 'e' => 'orange', 'f' => 'pine apple'],
+      3 => ['g' => 'water melon', 'h' => 'brinjal', 'i' => 'potato'],
+    ];
+
+    return [
+      '#theme' => 'day19_twig_test',
+      '#title' => $this->t('Test Title'),
+      '#var1' => $this->t('Test Description'),
+      '#var2' => $some_array,
+    ];
+  }
+
+}
