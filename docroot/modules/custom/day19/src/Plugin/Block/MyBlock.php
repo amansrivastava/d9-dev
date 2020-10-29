440181a0a94a1cef9680bf9d26aa45febdf2b5bd
Aman Srivastava <aman.srivastava@live.com>
2020-10-29 22:03:49 +0530

    D9-19: Add given module to code.

--- docroot/modules/custom/day19/src/Plugin/Block/MyBlock.php
+++ docroot/modules/custom/day19/src/Plugin/Block/MyBlock.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\day19\Plugin\Block\MyBlock.
- */
-
 namespace Drupal\day19\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
@@ -19,24 +14,23 @@ use Drupal\Core\Block\BlockBase;
  */
 class MyBlock extends BlockBase {
 
-
   /**
    * {@inheritdoc}
    */
   public function build() {
 
-    $some_array = array(
-      1 => array('a'=>'mango', 'b'=>'guava', 'c'=>'jack fruit'),
-      2 => array('d'=>'banana', 'e'=>'orange', 'f'=>'pine apple'),
-      3 => array('g'=>'water melon', 'h'=>'brinjal', 'i'=>'potato'),
-    );
+    $some_array = [
+      1 => ['a' => 'mango', 'b' => 'guava', 'c' => 'jack fruit'],
+      2 => ['d' => 'banana', 'e' => 'orange', 'f' => 'pine apple'],
+      3 => ['g' => 'water melon', 'h' => 'brinjal', 'i' => 'potato'],
+    ];
 
-    return array(
+    return [
       '#theme' => 'day19_twig_test',
       '#title' => $this->t('Test Title'),
       '#var1' => $this->t('Test Description'),
       '#var2' => $some_array,
-    );
+    ];
   }
 
 }
